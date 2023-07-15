7b7904e64012479c89b6e589eadba2253cc90a2b
Tristan Keester <tristankeester@gmail.com>
2023-07-14 19:20:29 -0500

    Boilerplate pretty much completed

---
+++ client/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import MainContainer from './containers/MainContainer.jsx';
+
+// import './stylesheets/styles.css';
+
+const App = () => {
+  return(
+    <div>
+      {/* <MainContainer/> */}
+      <p>THE PAGE LOADED</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.join(__dirname, "/dist"), // the bundle output path
+    filename: "bundle.js", // the name of the bundle
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "client/index.html", // to import index.html file inside index.js
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'build'),
+    },
+    proxy: {
+      '/': 'http://localhost:3000',
+    },
+    port: 8080, // you can change the port
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // .js and .jsx files
+        exclude: /node_modules/, // excluding the node_modules folder
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              ['@babel/preset-env', {targets: "defaults"}],
+              ['@babel/preset-react', {targets: "defaults"}],
+            ]
+          }
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/, // styles files
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
+        loader: "url-loader",
+        options: { limit: false },
+      },
+    ],
+  },
+};
