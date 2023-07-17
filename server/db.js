c80051582b0542ff64585dad792f5f97483391a9
gschussler <grantschussler@gmail.com>
2023-07-17 13:48:35 -0500

    set up routing and low-level auth to db

--- server/db.js
+++ server/db.js
@@ -1,16 +1,28 @@
+/*
+** Database connection through Supabase **
+
+import 'postgres' npm module
+env variables highly recommended; in this case,
+was imported in server.js
+*/
 const postgres = require('postgres');
 
+//'postgres' npm module and Supabase URL to prepare pool connection for export
 const connectionString = process.env.DATABASE_URL;
-console.log(process.env.DATABASE_URL)
+// console.log(process.env.DATABASE_URL)
 const sql = postgres(connectionString);
 
-// async function getUser() {
-//   const user = await sql`
-//     SELECT username
-//     FROM users
-//     WHERE username = 'guh';
-//   `
-//   return user;
-// }
+/*
+** Example SQL query with Supabase pool syntax **
+
+async function getUser() {
+  const user = await sql`
+    SELECT username
+    FROM users
+    WHERE username = 'USERNAME_HERE';
+  `
+  return user;
+}
+*/
 
 module.exports = sql;
